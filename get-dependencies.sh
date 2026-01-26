188ed3770356d47c65c2fbb1c1397debe60d12af
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-26 15:31:51 -0400

    use upstream binary directly

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,37 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-#pacman -Syu --noconfirm
+pacman -Syu --noconfirm \
+	fontconfig \
+	freetype2  \
+	harfbuzz   \
+	libx11     \
+	libxext    \
+	pango      \
+	pixman     \
+	unzip      \
+	zlib
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common ffmpeg-mini
 
 # Comment this out if you need an AUR package
-make-aur-package unleashedrecomp-bin
+make-aur-package flatpak-extract
 
-# If the application needs to be manually built that has to be done down here
+echo "Getting binary..."
+echo "---------------------------------------------------------------"
+BINARY=https://github.com/hedge-dev/UnleashedRecomp/releases/latest/download/UnleashedRecomp-Flatpak.zip
+if ! wget --retry-connrefused --tries=30 "$BINARY" 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+unzip "${BINARY##*/}"
+flatpak-extract ./*.flatpak
+
+mkdir -p ./AppDir/bin
+mv -v ./*-flatpak/files/bin/UnleashedRecomp                    ./AppDir/bin
+mv -v ./*-flatpak/files/share/*.desktop                        ./AppDir
+mv -v ./*-flatpak/files/share/icons/hicolor/128x128/apps/*.png ./AppDir
+mv -v ./*-flatpak/files/share/icons/hicolor/128x128/apps/*.png ./.DirIcon
