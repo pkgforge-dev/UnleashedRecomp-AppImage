9840d6830b53f8168bc800eb699be5f94d6c3122
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-26 15:33:44 -0400

    deploy upstream bin

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,19 +3,16 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q unleashedrecomp-bin | awk '{print $2; exit}') # example command to get version of application here
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/128x128/apps/io.github.hedge_dev.unleashedrecomp.png
-export DESKTOP=/usr/share/applications/io.github.hedge_dev.unleashedrecomp.desktop
-export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
+export DEPLOY_SDL=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/UnleashedRecomp
+quick-sharun ./AppDir/bin/*
 
 # Additional changes can be done in between here
 
